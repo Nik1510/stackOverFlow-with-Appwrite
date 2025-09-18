d06bbf3d908345734c420649bead729512de080d
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-09-19 01:01:06 +0530

    connected with appwrite

--- src/models/name.ts
+++ src/models/name.ts
@@ -1,6 +1,6 @@
-export const db = "main-stackFlow"
+export const db = "main-stackflow"
 export const questionCollection = "questions"
 export const answerCollection = "answers"
 export const commentCollection = "comments"
-export const votesCollection = "votes"
+export const voteCollection = "votes"
 export const questionAttachmentBucket = "question-attachment"
--- src/models/server/dbSetup.ts
+++ src/models/server/dbSetup.ts
@@ -6,26 +6,28 @@ import createVoteCollection from "./vote.collection";
 
 import { databases } from "./config";
 
-export default async function getOrCreate() {
+export default async function getOrCreateDB(){
+  try {
+    await databases.get(db)
+    console.log("Database connection")
+  } catch (error) {
     try {
-        await databases.get(db);
-        console.log('Database connection')
+      await databases.create(db, db)
+      console.log("database created")
+      //create collections
+      await Promise.all([
+        createQuestionCollection(),
+        createAnswerCollection(),
+        createCommentCollection(),
+        createVoteCollection(),
+
+      ])
+      console.log("Collection created")
+      console.log("Database connected")
     } catch (error) {
-        try {
-            await databases.create(db,db)
-            console.log('database created')
-            // create collections
-            await Promise.all([
-                createQuestionCollection(),
-                createAnswerCollection(),
-                createCommentCollection(),
-                createVoteCollection()
-            ])
-            console.log("Collection created")
-            console.log("Database connected")
-        } catch (error) {
-            console.log("Error creating database ot collection ",error)
-        }
+      console.log("Error creating databases or collection", error)
     }
-    return databases;
+  }
+
+  return databases
 }
